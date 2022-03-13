e21a6a85e0bd8c11c1e4e2e9485360fa48101ea4
Rodrigo William <rwps1234@gmail.com>
2022-03-12 23:40:04 -0300

    adicionando funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
-const btns = document.querySelectorAll('[data-option]');
-const resultado = document.querySelector('.resultado');
-// console.log(btn)
+(() => {
+    const botoes = document.querySelectorAll('[data-option]');
+    const resultado = document.querySelector('.resultado');
 
-document.querySelector('[data-clean=C]').addEventListener('click', () => {
-    console.log('apaga')
-});
-document.querySelector('[data-clean=del]').addEventListener('click', () => {
-    console.log('delete')
-});
-
-btns.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const opcao = botao.dataset.option;
-        resultado.innerHTML += opcao;
+    document.querySelector('[data-clean=C]').addEventListener('click', () => {
+        resultado.innerHTML = '';
+    });
+    document.querySelector('[data-clean=del]').addEventListener('click', () => {
+        resultado.innerHTML = resultado.textContent.substring(0, resultado.textContent.length - 1);
     });
 
+    botoes.forEach(botao => {
+        botao.addEventListener('click', () => {
+            const opcao = botao.dataset.option;
+            resultado.innerHTML += opcao;
+        });
+    });
+
+    document.querySelector('[data-resultado=calcular]').addEventListener('click', () => {
+        resultado.innerHTML = eval(resultado.textContent);
+    });
 })
-function total() {
-    return resultado.innerHTML = eval(resultado.textContent);
-}
-document.querySelector('[data-resultado=calcular]').addEventListener('click', total);
+();
