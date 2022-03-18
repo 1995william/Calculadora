d13c1da9e2ce1afe64962ddf78d17f7753b8a378
Rodrigo William <rwps1234@gmail.com>
2022-03-18 20:55:43 -0300

    adiconando arquivo erro e tratando possivel erros

---
+++ assets/js/erros.js
@@ -0,0 +1,14 @@
+import { clean } from './limpaCampos.js';
+
+export function erro(resultado) {
+    if (resultado == 'Infinity' || resultado == 'NaN') {
+        return clean();
+    };
+};
+export function limiteDeCaracteres(digito) {
+    if (digito.length == 16) {
+        const resultado = document.querySelector('.h2');
+        const limite = resultado.substring(15, 16);
+        return limite;
+    };
+};
