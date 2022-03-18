a4bcf035badeb74cbbee289ddc8b25ec2a408bd0
Rodrigo William <rwps1234@gmail.com>
2022-03-18 20:59:04 -0300

    importanto módulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
+import {del,clean} from './limpaCampos.js';
+import {erro, limiteDeCaracteres} from './erros.js';
 (() => {
+    const resultado = document.querySelector('.h2');
+    // evento que identifica o botao Clean
+    document.querySelector('[data-clean=C]').addEventListener('click',clean);
+    
+    // evento que identifica o botao Del
+    document.querySelector('[data-clean=del]').addEventListener('click',del);
+    
+    // evento que identifica os dígito numérico e operação
     const botoes = document.querySelectorAll('[data-option]');
-    const resultado = document.querySelector('.resultado');
-
-    document.querySelector('[data-clean=C]').addEventListener('click', () => {
-        resultado.innerHTML = '';
-    });
-    document.querySelector('[data-clean=del]').addEventListener('click', () => {
-        resultado.innerHTML = resultado.textContent.substring(0, resultado.textContent.length - 1);
-    });
-
     botoes.forEach(botao => {
         botao.addEventListener('click', () => {
-            const opcao = botao.dataset.option;
-            resultado.innerHTML += opcao;
+            erro(resultado.textContent);
+            limiteDeCaracteres(resultado.textContent);
+            const digito = botao.dataset.option;
+            resultado.innerHTML += digito;
         });
     });
 
+    // evento que executa o resultado da operação
     document.querySelector('[data-resultado=calcular]').addEventListener('click', () => {
         resultado.innerHTML = eval(resultado.textContent);
     });
