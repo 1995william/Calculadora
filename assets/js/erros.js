9e9f031c56d27612304827e4125114a78df32c40
Rodrigo William <rwps1234@gmail.com>
2022-03-19 20:07:25 -0300

    criando função erroNaN

--- assets/js/erros.js
+++ assets/js/erros.js
@@ -3,14 +3,20 @@ import { clean } from './limpaCampos.js';
 export function erroDeCalculo (resultado) {
     if (resultado == 'Infinity' || resultado == 'NaN' || resultado == 'enter a value...') {
         return clean();
-    }
+    };
 };
+export function erroNaN (resultado) {
+    if(resultado == 'NaN') {
+        const resultado = document.querySelector('.h2');
+        resultado.innerHTML = 'Infinity';
+    };
+}
 export function erroSemDigito (resultado) {
     if(resultado == 'undefined') {
         const resultado = document.querySelector('.h2');
         resultado.innerHTML = 'enter a value...';
-    }
-}
+    };
+};
 export function limiteDeCaracteres(digito) {
     if (digito.length == 12) {
         const resultado = document.querySelector('.h2');
